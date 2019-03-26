61fa370ff875926bddf39e6610d50c06775bd221
camelboat <camelboat@sjtu.edu.cn>
2019-03-27 05:54:31 +0800

    modified:   thingspeak_channel_update.py

--- thingspeak_channel_update.py
+++ thingspeak_channel_update.py
@@ -1,21 +1,41 @@
 import sys
 from time import sleep
-from urllib.request import urlopen
+#from urllib.request import urlopen
+import requests
 
 baseURL = 'https://api.thingspeak.com/update?api_key=T9PJ3W9K7NSQ6AT8&field1=0'
 
-a=0
-b=1
-c=0
+baseURL_temperature_room1 = 'https://api.thingspeak.com/update?api_key=LWV7LOPF9QN79QG4&field1=0'
+baseURL_temperature_PIR1 = 'https://api.thingspeak.com/update?api_key=LWV7LOPF9QN79QG4&field2=0'
+baseURL_temperature_setting_room1 = 'https://api.thingspeak.com/update?api_key=LWV7LOPF9QN79QG4&field1=0'
+baseURL_temperature_setting_room2 = 'https://api.thingspeak.com/update?api_key=LWV7LOPF9QN79QG4&field2=0'
 
-while(a < 1000):
-    f = urlopen(baseURL + str(a))
-    f.read()
-    f.close()
-    sleep(5)
-    c = a
-    a = a+b
-    b = c
-print(over)
+'''
+# Update temperature data for room1
+temperature = 25
+f = requests.get(baseURL_temperature_room1 + str(temperature))
+print(f.content)
+sleep(1)
+
+# Update PIR_1 data for room1
+PIR = 0
+f = requests.get(baseURL_temperature_PIR1 + str(PIR))
+print(f.content)
+sleep(1)
+'''
+
+# Update temperature setting for room1
+temperature_setting = 25
+f = requests.get(baseURL_temperature_setting_room1 + str(temperature_setting))
+print(f.content)
+sleep(1)
+
+# Update temperature setting for room2
+temperature_setting = 25
+f = requests.get(baseURL_temperature_setting_room2 + str(temperature_setting))
+print(f.content)
+sleep(1)
+
+print("over")
 
 
